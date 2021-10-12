ec3228d2665c7ead108c9406df6be379f6b771cf
AidenH <aidenalexh@gmail.com>
2021-10-12 13:08:29 -0400

    Reformat ElectionCaller

--- contracts/Electio.sol
+++ contracts/Electio.sol
@@ -1,6 +1,33 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.9;
 
+contract ElectionCaller {
+    address[] electionList;
+    address private elecAddr;
+    address private owner;
+
+    modifier initPrice {
+        require(msg.value >= 2, "Not enough Eth available to call vote.");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function createElection() public payable initPrice returns (address) {
+        elecAddr = address(new Election(msg.sender));
+        electionList.push(elecAddr);
+
+        return elecAddr;
+    }
+
+    function callVote() public returns (string memory) {
+        // string memory result = Election(elecAddr).placeVote(msg.sender, );
+        // return result;
+    }
+}
+
 contract Election {
     mapping(address => uint256) private candidateList;
     address[] voterList;
@@ -21,17 +48,13 @@ contract Election {
 
     function placeVote(address _callerAddr, address _candidate) public votingPeriod returns (string memory) {
         if (block.timestamp < (block.timestamp + registrationTime) + votingTime) {
-        voterList.push(_callerAddr);
+            voterList.push(_callerAddr);
             candidateList[_candidate] += 1;
 
-        return "vote placed!";
-    }
+            return "vote placed!";
+        }
         else {
-
-    }
-
-    function callVote() public returns (string memory) {
-        string memory result = Election(elecAddr).placeVote(msg.sender);
-        return result;
+            
+        }
     }
 }
