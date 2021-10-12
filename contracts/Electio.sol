fe0b6d33f7ec922618bd5c7d58eab404e3fa5bb8
AidenH <aidenalexh@gmail.com>
2021-10-12 13:07:51 -0400

    Add voting periods

--- contracts/Electio.sol
+++ contracts/Electio.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.9;
 contract Election {
     mapping(address => uint256) private candidateList;
     address[] voterList;
+    uint256 elecStart;
+    uint256 registrationTime = 60;
+    uint256 votingTime = 60;
+
     modifier votingPeriod {
         require(block.timestamp > elecStart + registrationTime, 
             "Registration period still active.");
@@ -12,28 +16,18 @@ contract Election {
 
     constructor(address _callerAddr) {
         candidateList[_callerAddr] = 0;
+        elecStart = block.timestamp;
     }
 
     function placeVote(address _callerAddr, address _candidate) public votingPeriod returns (string memory) {
+        if (block.timestamp < (block.timestamp + registrationTime) + votingTime) {
         voterList.push(_callerAddr);
+            candidateList[_candidate] += 1;
 
         return "vote placed!";
     }
-}
+        else {
 
-contract Caller {
-    address[] electionList;
-    address private elecAddr;
-
-    constructor() payable {
-
-    }
-
-    function createElection() public returns (address) {
-        elecAddr = address(new Election(msg.sender));
-        electionList.push(elecAddr);
-
-        return elecAddr;
     }
 
     function callVote() public returns (string memory) {
