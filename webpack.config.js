2873b18228e22615d69b39a1040af980928c5fce
AidenH <aidenalexh@gmail.com>
2021-10-18 13:21:42 -0400

    Resolve source map and hot-loader problems

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,23 @@ const webpack = require("webpack")
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
+	entry: "./src/index.js",
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: "babel-loader",
+				options: { presets: ["@babel/env"] }
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+		],
+	},
+	resolve: {
 		extensions: ["*", ".js", ".jsx", ".ts"],
 		fallback: {
 			"os": require.resolve("os-browserify/browser"),
@@ -29,15 +30,26 @@ module.exports = {
 			"stream": require.resolve("stream-browserify"),
 			"buffer": require.resolve("buffer"),
 		},
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    static: path.join(__dirname, "src/components/"),
-    port: 3000,
-    allowedHosts: 'auto'
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+		alias: { 'react-dom': '@hot-loader/react-dom' },
+	},
+	output: {
+		path: path.resolve(__dirname, "dist/"),
+		publicPath: "/dist/",
+		filename: "bundle.js"
+	},
+	devServer: {
+		static: path.join(__dirname, "src/components/"),
+		port: 3000,
+		allowedHosts: 'auto'
+	},
+	devtool: "source-map",
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.ProvidePlugin({
+			Buffer: ['buffer', 'Buffer'],
+		}),
+		new webpack.ProvidePlugin({
+			process: 'process/browser'
+		}),
+	],
 };
