306abfdfc8b2b1d541fcc6890e29d91824c046eb
AidenH <aidenalexh@gmail.com>
2021-10-28 12:04:15 -0400

    Add future address return location

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,13 +4,39 @@ import Web3 from "web3"
 
 const web3 = new Web3.providers.WebsocketProvider("ws:localhost:3000")
 
+const addrDiv = document.getElementById("return-election-address")
+
 class Body extends Component {
+    constructor() {
+        super()
+        this.state = {
+            addr: "AAAAAA",
+            addrVisible: false
+        }
+    }
+
     render() {
         return (
             <div id="app-body">
-                <div className="app-button">Body</div>
+                <div className="app-button" onClick={() => {
+                    this.addrUpdate()
+                }}>Body</div>
+
+                {this.state.addrVisible &&
+                    <div id="return-election-address">
+                        <p>Your Election instance address is:</p>
+                        {this.state.addrVisible && this.state.addr}
+                    </div>
+                }
             </div>
         )
+    }
+
+    addrUpdate() {
+        this.setState({
+            addr: "BBBBB",
+            addrVisible: true
+        })
     }
 }
 
