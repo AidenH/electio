2ba3444ee6450324486f8fb256c161c3051b74d0
AidenH <aidenalexh@gmail.com>
2021-11-02 20:03:36 -0400

    Modify createElection for access

--- src/contracts/ElectionCaller.sol
+++ src/contracts/ElectionCaller.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.9;
 import "./Election.sol";
 
 contract ElectionCaller {
-    address[] electionList;
-    address private elecAddr;
+    address[] public electionList;
+    address public elecAddr;
     address private owner;
 
     // Only charge to create election
@@ -19,11 +19,11 @@ contract ElectionCaller {
         owner = msg.sender;
     }
 
-    function createElection() public payable initPrice(0 gwei) returns (address[] memory) {
-        //elecAddr = address(new Election(msg.sender));
-        electionList.push(address(new Election(msg.sender)));
+    function createElection() public payable initPrice(0 gwei) returns (address) {
+        elecAddr = address(new Election(msg.sender));
+        electionList.push(elecAddr);
 
-        return electionList;
+        return elecAddr;
     }
 
     // -For later use-
