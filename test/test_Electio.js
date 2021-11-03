70a3136a74de1ec25c4012a605425fff0c41d0a5
AidenH <aidenalexh@gmail.com>
2021-11-02 20:03:02 -0400

    Write tests

--- test/test_Electio.js
+++ test/test_Electio.js
@@ -1,13 +1,32 @@
+const assert = require("assert")
+
 const ElectionCaller = artifacts.require("ElectionCaller")
 const Election = artifacts.require("Election")
 
 contract("ElectionCaller", () => {
-    // createElection()
-    it("caller should return address", async() => {
-        const inst = await ElectionCaller.deployed()
-        const result = await inst.createElection.call()
 
-        console.log("LOG:", result)
-        //assert.equal(a, 4)
+    // Deploy contract
+    it("Should deploy ElectionCaller contract", async() => {
+        const isDeployed = await ElectionCaller.isDeployed()
+        assert.equal(isDeployed, true)
     })
-}) 
+
+    // createElection()
+    it("Should return address", async() => {
+        const inst = await ElectionCaller.deployed()
+        
+        await inst.createElection()
+        let varResult = await inst.createElection.call()
+
+        assert.equal(varResult.substring(0,2), "0x")
+    })
+})
+
+contract("Election", () => {
+
+    // Deployment contract
+    it("Should deploy Election contract", async() => {
+        const isDeployed = await ElectionCaller.isDeployed()
+        assert.equal(isDeployed, true)
+    })
+})
