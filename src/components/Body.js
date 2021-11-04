38da8f13e771e2c308ff8d466390b20daa1037f2
AidenH <aidenalexh@gmail.com>
2021-11-04 14:08:01 -0400

    Edit const formatting

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,12 +3,10 @@ import { AppContainer, hot } from "react-hot-loader"
 import Web3 from "web3"
 import ElectionCallerAbi from '../../build/contracts/ElectionCaller.json'
 
-// Instantiate web3 and deployed ElectionCaller contract
+// Init web3 and deployed ElectionCaller contract
 const web3 = new Web3('ws://127.0.0.1:8545')
-
 const contractAddr = web3.utils.toChecksumAddress(
-    "0x9A84ae7f5022da413B363b7888CE0C8E10eFaF8b"
-)
+    "0x9A84ae7f5022da413B363b7888CE0C8E10eFaF8b")
 const ElectionCaller = new web3.eth.Contract(ElectionCallerAbi.abi, contractAddr)
 
 // Check for MetaMask
