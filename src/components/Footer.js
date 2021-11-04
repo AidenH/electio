4afbdfcb05f4220e9f984d6c5307def394cb326f
AidenH <aidenalexh@gmail.com>
2021-11-04 17:20:54 -0400

    Add self address  to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react"
 import { hot } from "react-hot-loader"
+import Web3 from "web3"
 
 class Footer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            selfAddr: "nil",
+        }
+        this.getselfAddr()
+    }
+
+    async getselfAddr() {
+        const _selfAddr = await ethereum.request({method: 'eth_requestAccounts'})
+        this.setState({
+            selfAddr: _selfAddr,
+        })
+    }
+
     render() {
         return (
             <div id="app-footer">
-                Footer
+                Your address is: {this.state.selfAddr}
             </div>
         )
     }
